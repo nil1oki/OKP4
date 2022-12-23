50fcff2c4cfa215ca8bc39ee6f7254b73b366651
Ruslan <102181017+nil1oki@users.noreply.github.com>
2022-12-23 19:17:02 +0500

    Add files via upload

---
+++ autostake1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+WITHOU_COLOR='\033[0m'
+DELEGATOR_ADDRESS='okp414u09zxsxw752dfrzy7g4zxj6d4mprv4rmyv6lt'
+VALIDATOR_ADDRESS='okp4valoper14u09zxsxw752dfrzy7g4zxj6d4mprv4rwrunp2'
+DELAY=86400 #in secs - how often restart the script 
+ACC_NAME=wallet #example: = ACC_NAME=wallet_qwwq_54
+NODE="tcp://localhost:26657" #change it only if you use another rpc port of your node
+CHAIN_NAME=okp4-nemeton-1
+
+for (( ;; )); do
+        echo -e "Get reward from Delegation"
+        echo -e "gkksdfdv5568GHHhg" | okp4d tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id ${CHAIN_NAME} --from ${ACC_NAME} --gas=auto --fees 5000uknow --commission --node ${NODE} --yes
+
+        for (( timer=30; timer>0; timer-- ))
+        do
+                printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
+                sleep 1
+        done
+ 
+        BAL=$(okp4d q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount')
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uknow\n"
+
+        echo -e "Claim rewards\n"
+        echo -e "gkksdfdv5568GHHhg" | okp4d tx distribution withdraw-all-rewards --from ${ACC_NAME} --chain-id ${CHAIN_NAME} --gas=auto --fees 5000uknow --node ${NODE} --yes
+        for (( timer=30; timer>0; timer-- ))
+        do
+                printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
+                sleep 1
+        done
+        BAL=$(okp4d q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
+        BAL=$(($BAL-1000000))
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uknow\n"
+        echo -e "Stake ALL\n"
+        if (( BAL > 1000000 )); then
+        echo -e "gkksdfdv5568GHHhg" | okp4d tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}uknow --from ${ACC_NAME} --gas=auto --fees 5000udws --node ${NODE} --chain-id ${CHAIN_NAME}  --yes
+        else
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uknow BAL < 10000000 ((((\n"
+        fi
+        for (( timer=${DELAY}; timer>0; timer-- ))
+        do
+                printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
+                sleep 1
+        done       
+
+done
